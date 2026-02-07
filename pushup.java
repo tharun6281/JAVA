3cbc761ca39641ae5939540c5de893ebee67c3c0
M Tharun <tharungowda502@gmail.com>
2026-02-07 16:16:03 +0530

    Add files via upload

---
+++ pushup.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class pushup
+{
+	public static void main (String[] args)
+	{
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter number of pushups in a day:");
+        int x=sc.nextInt();
+        int chef=x*31;
+        System.out.println(chef);
+        System.out.println("per month.");
+	}
+}
