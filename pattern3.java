b12c84d7ee6deba32f82fc02ff84b7421c1996dc
M Tharun <tharungowda502@gmail.com>
2026-02-28 11:53:52 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,20 @@
+public class pattern2
+{
+    public static void main(String[] args)
+    {
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-1;j++)
+            {
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
